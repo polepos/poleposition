50813cc118ab9bee9463030a43a10fa34c433970
Eren Ertem <mail.erenertem@gmail.com>
2026-04-26 15:34:52 +0300

    refactor: use command registry in CLI entrypoint

---
+++ pole_position/cli/commands/__init__.py
@@ -0,0 +1,9 @@
+from pole_position.cli.registry import registry
+
+from pole_position.cli.commands.startproject import command as start_cmd
+from pole_position.cli.commands.version import command as version_cmd
+
+
+def register_commands() -> None:
+    registry.register(start_cmd)
+    registry.register(version_cmd)
